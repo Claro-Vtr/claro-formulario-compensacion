6a642919dcca7d0abdd6056e6414f57abc302b5a
Carlos Orrego <orregoc@globalhitss.com>
2025-05-23 11:05:17 -0400

    msg : test

--- src/Views/InvalidRegistration.tsx
+++ src/Views/InvalidRegistration.tsx
@@ -10,7 +10,6 @@ const InvalidRegistration = ({
   return (
     <div className="w-full mt-8 mb-8 p-6 border rounded-xl text-center">
       <img src={Img} alt="Registro Fallido" className="mx-auto" />
-      {/* <h1 className="mt-6 font-medium text-2xl text-[#AF272F]">Hubo un error al insertar los datos.</h1> */}
       <h1 className="mt-6 font-medium text-2xl text-[#AF272F]">{mensaje}</h1>
       <div className="flex flex-col sm:flex-row justify-center gap-4 mt-8">
         <button
